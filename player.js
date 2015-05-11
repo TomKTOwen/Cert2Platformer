46ad21616962632d7309c1c71e6086ec27d755b0
Tom Owen <tom-tom_1989@hotmail.com>
2015-05-11 19:28:19 +1000

    Update player.js
    updating
    ++ organizing File

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
-var LEFT = 0;
-var RIGHT = 1; 
-var JUMP = 2
+var LEFT 	= -1;
+var RIGHT 	= 1; 
+
 
 var ANIM_IDLE_LEFT 	    = 0; 
 var ANIM_JUMP_LEFT 	    = 1;
@@ -13,13 +13,14 @@ var ANIM_WALK_RIGHT	    = 5;
 var ANIM_MAX 			= 6;      //for now
 
 
-var Player = function() {
+var Player = function() 
+{
 	//drawing player with each sprite plus each run fun function
 	this.sprite = new Sprite("ChuckNorris.png");
 	
 	//idle left
 	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
-		[0,1,2,3,4,5,6,7]);
+		[0, 1, 2, 3, 4, 5, 6, 7]);
 	//idle jump
 	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
 		[8, 9, 10, 11, 12]);	
@@ -28,7 +29,7 @@ var Player = function() {
 		[13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
 	//idle right
 	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
-		[52, 53, 54, 55, 56, 57, 58, 59]);
+		[52, 53, 54, 55, 56, 57, 58]);
 	//jump right
 	this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
 		[60, 61, 62, 63, 64]);
@@ -38,14 +39,17 @@ var Player = function() {
 		
 	for (var i = 0; i < ANIM_MAX; ++i)
 	{
-		this.sprite.setAnimationOffset(i, -55, -87);
+		this.sprite.setAnimationOffset(i, -55, -88);
 	}
 	
-	this.position = new Vector2();
-	this.position.set(9 * TILE, 0 * TILE);
-	
-	this.width = 159;
-	this.height = 163;
+	this.position 	= new Vector2();
+	this.position.x = 11 * TILE;
+	this.position.y = 7 * TILE;
+	this.width 		= 159;
+	this.height 	= 163;
+
+	this.offset = new Vector2();
+	this.offset.set(-55, -87);
 	
 	this.velocity = new Vector2();
 	
@@ -53,7 +57,7 @@ var Player = function() {
 	this.jumping = false;
 	
 };
-
+var keyPress = 0;
 Player.prototype.update = function(deltaTime)
 {
 	this.sprite.update(deltaTime)
@@ -62,153 +66,202 @@ Player.prototype.update = function(deltaTime)
 	var right 	 = false;
 	var jump     = false;
 	
-	//check key press events
-	if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true) 
-	{
-		left = true;
-		this.direction = LEFT;
-		if(this.sprite.currentAnimation != ANIM_WALK_LEFT && this.jumping == false && this.falling == false)
-			this.sprite.setAnimation(ANIM_WALK_LEFT);
-	}
-
-	else if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true)
-	{
-		right = true;
-		this.direction = RIGHT;
-		if(this.sprite.currentAnimation != ANIM_WALK_RIGHT && this.jumping == false && this.falling == false)
-			this.sprite.setAnimation(ANIM_WALK_RIGHT);
-	}
-	
-	if (keyboard.isKeyDown(keyboard.KEY_SPACE) == true)
-	{
-		jump = true;
-		this.direction = JUMP;
-		if(this.sprite.currentAnimation != ANIM_JUMP_LEFT && this.jumping == true && this.falling == true)
-			this.sprite.setAnimation(ANIM_JUMP_LEFT);
-		if(this.sprite.currentAnimation != ANIM_JUMP_RIGHT && this.jumping == true && this.falling == true)
-			this.sprite.setAnimation(ANIM_JUMP_RIGHT);	
-	}
-	
-	
-	else
-	{
-		if (this.jumping == false && this.falling == false)
+	if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true)
 		{
-			if (this.direction == LEFT)
-			{
-				if(this.sprite.currentAnimation != ANIM_IDLE_LEFT)
-				this.sprite.setAnimation(ANIM_IDLE_LEFT);
+			left = true;
+			this.direction = LEFT;
+			if (this.Sprite.currentAnimation != ANIM_WALK_LEFT &&
+			this.jumping == false &&
+			this.falling == false ){
+			this.Sprite.setAnimation(ANIM_WALK_LEFT)
 			}
-			else
-			{
-				if(this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
-				this.sprite.setAnimation(ANIM_IDLE_RIGHT);
-			}
-
-		}
-	}
-	
-	var wasLeft	 = this.velocity.x < 0; 
-	var wasRight = this.velocity.x > 0;
-	var falling	 = this.falling;
-	
-	var accelerationx = 0;
-	var accelerationy = GRAVITY;
-	
-	if (left)
-		accelerationx = accelerationx - ACCEL;
-	else if (wasLeft) 
-		accelerationx = accelerationx + FRICTION;
-		
-	if (right)
-		accelerationx = accelerationx + ACCEL;
-	else if (wasRight) 
-		accelerationx = accelerationx + FRICTION;
-
-    if (jump && !this.jumping && !falling) 
-	{
-		accelerationy = accelerationy - JUMP;
-		this.jumping = true;
-		if (this.direction == LEFT)
-			this.sprite.setAnimation(ANIM_JUMP_LEFT)
-		else
-			this.sprite.setAnimation(ANIM_JUMP_RIGHT)
-	}
-	
-	//add our current velocity
-	this.position.y = Math.floor(this.position.y + (deltaTime * this.velocity.y));
-	this.position.x = Math.floor(this.position.x + (deltaTime * this.velocity.x));
-	
-	this.velocity.x = bound(this.velocity.x + (deltaTime * accelerationx), -MAXDX, MAXDX);
-	this.velocity.y = bound(this.velocity.y + (deltaTime * accelerationy), -MAXDY, MAXDY);
-	
-	if (wasLeft && (this.velocity.x > 0) ||
-	   (wasRight && (this.velocity.x < 0)))
-	  {
-		this.velocity.x = 0;
-	  }
-		
-	////////////////////////////////////////////////////////////////////////////////
-	////getting out tiles closest to the player
-	var tx = pixelToTile(this.position.x);
-	var ty = pixelToTile(this.position.y);
-	
-	var nx = (this.position.x)%TILE;
-	var ny = (this.position.y)%TILE;
-	
-	var cell = cellAtTileCoord(LAYER_PLATFORMS, tx, ty);
-	var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
-	var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
-	var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
-	
-	////////////////////////////////////////////////////////////////////////////////
-	//check collision
-	if (this.velocity.y > 0)
-	{
-		//checks downward position
-		if ((celldown && !cell) || (celldiag && !cellright && nx))
-		{
-			this.position.y = tileToPixel(ty);
-			this.velocity.y = 0;
-			this.falling = false;
-			this.jumping = false;
-			ny = 0; //no longer overlapping the sell below
-		}
-	}
-	//upwards
-	else if (this.velocity.y < 0)
-	{
-		if ((cell && !celldown) || (cellright && !celldiag && nx))
-		{
-			this.position.y = tileToPixel(ty + 1)
-			this.velocity.y = 0; 
 			
-			cell = celldown;
-			cellright = celldiag;
-			ny = 0; 
 		}
-	}
-	//right
-	if (this.velocity.x > 0)
-	{
-		if ((cellright && !cell) || (celldiag && !celldown && ny))
+		if (keyboard.isKeyDown(keyboard.KEY_RIGHT) == true){
+			right = true;
+			this.direction = RIGHT;
+			if (this.Sprite.currentAnimation != ANIM_WALK_RIGHT &&
+			this.jumping == false &&
+			this.falling == false ){
+			this.Sprite.setAnimation(ANIM_WALK_RIGHT)
+			}
+			
+		}
+		
+		if (keyboard.isKeyDown(keyboard.KEY_SPACE) == true && right)
 		{
-			this.position.x = tileToPixel(tx);
-			this.velocity.x = 0; 
+			jump = true;
+			if (this.Sprite.currentAnimation != ANIM_JUMP_LEFT)
+			{
+				this.Sprite.setAnimation(ANIM_JUMP_RIGHT)
+			}
+			
 		}
-	}
-	//left
-	else if (this.velocity.x < 0)
-	{
-		if ((cell && !cellright) || (celldown && !celldiag && ny)) 
+		
+		if (keyboard.isKeyDown(keyboard.KEY_SPACE) == true)
 		{
-			this.position.x = tileToPixel(tx + 1);
-			this.velocity.x = 0; 
+			jump = true;
+			if(this.left == true)
+			{
+				this.Sprite.setAnimation(ANIM_JUMP_LEFT)
+			}
+			keyPress++
+			
 		}
-	}
-};
+		
+		if (keyboard.isKeyDown(keyboard.KEY_SPACE) == true && left)
+		{
+			jump = true;
+			if (this.Sprite.currentAnimation != ANIM_JUMP_LEFT)
+			{
+				this.Sprite.setAnimation(ANIM_JUMP_LEFT)
+			}
+			
+		}
+		
+		if (jump == false && this.velocity.x == 0 && this.velocity.y == 0 && keyPress > 0)
+		{
+			this.Sprite.setAnimation(ANIM_IDLE_LEFT)
+		}
+		
+		if (jump == true && this.velocity.x == 0)
+		{
+			this.Sprite.setAnimation(ANIM_JUMP_LEFT) 
+		}
+		
+		if (jump == true && this.velocity.x == 0)
+		{
+			this.Sprite.setAnimation(ANIM_JUMP_LEFT)
+		}
+		
+		if (jump == true && keyPress == 0 && this.currentAnimation != ANIM_IDLE_LEFT)
+		{
+			this.Sprite.setAnimation(ANIM_IDLE_LEFT)
+		}
+		
+	
+		
+		if (jump == false && this.velocity.x == 0 && this.direction == LEFT)
+		{
+			this.Sprite.setAnimation(ANIM_IDLE_LEFT)
+		};
+		
+		if (this.velocity.x == 0 && this.direction == LEFT)
+		{
+			this.Sprite.setAnimation(ANIM_IDLE_LEFT)
+		}
+			
+		if (this.velocity.x == 0 && this.direction == RIGHT)
+		{
+			this.Sprite.setAnimation(ANIM_IDLE_RIGHT)
+		}
+		
+		if (this.velocity.y == 0 && this.velocity.x == 0 && this.direction == RIGHT)
+		{
+			this.Sprite.setAnimation(ANIM_IDLE_RIGHT)
+		};
+	
+		
+		var wasLeft = this.velocity.x < 0;
+		var wasRight = this.velocity.x > 0;
+		var falling = this.falling;
+		var acceleration = new Vector2();
+			acceleration.y = GRAVITY;
+		
+		if (left)
+			acceleration.x += BACCEL;
+		else if (wasLeft)
+			acceleration.x += FRICTION;
+			
+		if (right)
+			acceleration.x += ACCEL;
+		else if (wasRight)
+			acceleration.x -= FRICTION;
+		
+		if ( jump && !this.jumping && !falling)
+		{
+			acceleration.y += JUMP;
+			this.jumping -= true;
+		};
+		
+		// adds our current velocity
+		
+		this.position.y = Math.floor(this.position.y + (deltaTime * this.velocity.y));
+		this.position.x = Math.floor(this.position.x + (deltaTime * this.velocity.x));
+		
+		this.velocity.x = bound(this.velocity.x + (deltaTime * acceleration.x), -MAXDX, MAXDX);
+		this.velocity.y = bound(this.velocity.y + (deltaTime * acceleration.y), -MAXDY, MAXDY);
 
-Player.prototype.draw = function(context)
-{
-	this.sprite.draw(context, this.position.x - worldOffsetX, this.position.y);
+		if (wasLeft && (this.velocity.x > 0) || 
+			wasRight && (this.velocity.x < 0))
+			this.velocity.x = 0;
+		
+		var Tx = pixelToTile(this.position.x);
+		var Ty = pixelToTile(this.position.y);
+		
+		var nx = (this.position.x) % TILE;
+		var ny = (this.position.y) % TILE;
+		
+		var cell = cellAtTileCoord(LAYER_PLATFORMS, Tx, Ty);
+		var cellUp = cellAtTileCoord(LAYER_PLATFORMS, Tx, Ty +1);
+		var cellRight = cellAtTileCoord(LAYER_PLATFORMS, Tx + 1, Ty);
+		var cellLeft = cellAtTileCoord(LAYER_PLATFORMS, Tx - 1, Ty);
+		var cellDown = cellAtTileCoord(LAYER_PLATFORMS, Tx, Ty + 1);
+		var cellDiag = cellAtTileCoord(LAYER_PLATFORMS, Tx + 1, Ty + 1);
+		
+		//////////////////////////
+		//// check collision ////
+		////  downwards		////
+		///////////////////////
+		if (this.velocity.y > 0)
+		{
+			if((cellDown && !cell) || (cellDiag && !cellRight && nx ))
+			{
+				this.position.y = tileToPixel(Ty);
+				this.velocity.y = 0;
+				this.falling = false;
+				this.jumping = false;
+				this.land = true;
+				ny = 0; // no longer overlapping the cell below
+			}
+		}
+		////upwards
+		else if (this.velocity.y < 0)
+		{
+			if((cell && !cellDown) || (cellRight && !cellDiag && nx))
+			{
+				this.position.y = tileToPixel(Ty+ 1);
+				this.velocity.y = 0;
+				
+				cell = cellDown;
+				cellRight = cellDiag;
+				ny = 0;
+			}
+		}
+		////right
+		if (this.velocity.x > 0)
+		{
+			if((cellRight && !cell) || (cellRight && !cellDiag && ny))
+			{
+				this.position.x = tileToPixel(Tx);
+				this.velocity.x = 0; 
+			}
+		}
+	////left
+		if (this.velocity.x < 0)
+		{
+			if((cellLeft && !cell) || (cellLeft && !cellDiag && ny))
+			{
+			this.position.x = tileToPixel (Tx) + 1;
+			this.velocity.x = 0;
+			}
+		}
+	};
+
+Player.prototype.draw = function()
+{	
+	
+	this.Sprite.draw(context, this.position.x - worldOffsetX, this.position.y)
+	
+
 };
